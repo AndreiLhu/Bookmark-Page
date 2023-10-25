c7fa89e039459a36426514c77dd6b8a9a78c892d
AndreiLhu <leahu.andreilucian@gmail.com>
2023-10-25 11:40:26 +0200

    home styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,39 +1,24 @@
 import './Home.css';
-// import homeImage from '../../../images/illustration-hero.svg';
+import homeImage from '../../../images/illustration-hero.svg';
 const Home: React.FC = () => {
   return (
     <>
       <div className="mainHomeContainer" id="home">
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum omnis
-            delectus praesentium ipsa voluptates veniam est? Quae vitae delectus
-            fugit possimus quod aut blanditiis dolore et velit quo cumque
-            reprehenderit accusamus excepturi, voluptatibus ex repellendus vero
-            voluptate aspernatur doloribus minus a consequatur dolorem soluta
-            dolores? Blanditiis voluptatibus iure beatae minima.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum omnis
-            delectus praesentium ipsa voluptates veniam est? Quae vitae delectus
-            fugit possimus quod aut blanditiis dolore et velit quo cumque
-            reprehenderit accusamus excepturi, voluptatibus ex repellendus vero
-            voluptate aspernatur doloribus minus a consequatur dolorem soluta
-            dolores? Blanditiis voluptatibus iure beatae minima.
-          </p>
+        <div className="homeTextContainer">
           <h1> A Simple Bookmark Manager</h1>
           <p>
             A clean and simple interface to organize your favourite websites.
             Open a new browser tab and see your sites load instantly. Try it for
             free.
           </p>
-          <button>Get it on Chrome</button>
-          <button> Get it on Firefox </button>
+          <button className="chromeButton">Get it on Chrome</button>
+          <button className="firefoxButton"> Get it on Firefox </button>
         </div>
-        {/* <div>
+        <div className="homeImage">
           <img src={homeImage} alt="homeImage" />
-        </div> */}
+        </div>
       </div>
+      <div className="homeBackground"></div>
     </>
   );
 };
