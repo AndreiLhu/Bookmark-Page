14294f005869f5a403f4bf57a1f2d8f281e7e875
AndreiLhu <leahu.andreilucian@gmail.com>
2023-10-26 12:13:28 +0200

    questions opens with id first sketch working

--- src/pages/Questions/Questions.tsx
+++ src/pages/Questions/Questions.tsx
@@ -3,13 +3,13 @@ import './Questions.css';
 import data from '../../data/questions.json';
 import QuestionCard from '../../components/QuestionCard/QuestionCard';
 const Questions: React.FC = () => {
-  // const [isQuestionOpen, setIsQuestionOpen] = React.useState<boolean>(false);
+  const [isQuestionOpen, setIsQuestionOpen] = React.useState<boolean>(false);
   const [questionIndex, setQuestionIndex] = React.useState<number>(0);
   const { questions } = data;
   const onShowPlanetContent = React.useCallback((index: number) => {
     setQuestionIndex(index);
   }, []);
-  // const toggleQuestion = () => setIsQuestionOpen(!isQuestionOpen);
+  const toggleQuestion = () => setIsQuestionOpen(!isQuestionOpen);
 
   return (
     <div id="questions" className="mainQuestionsDivContainer">
@@ -21,35 +21,27 @@ const Questions: React.FC = () => {
         </p>
       </div>
       <div className="mainQuestionsDiv">
-        {/* <ul className="planetsNameButtonsContainer">
-          {questions.map((questionContent, index) => (
-            <button
-              key={index}
-              onClick={() => onShowPlanetContent(index)}
-              className="planetsNameButton"
-            >
-              <span onClick={() => toggleQuestion()}>
-                {questionContent.title}
-              </span>
-              {isQuestionOpen && (
-                <QuestionCard content={questionContent[index].content} />
-              )}
-            </button>
-          ))}
-        </ul> */}
         <div className="destinationInfoContainer">
           <ul className="planetsNameButtonsContainer">
             {questions.map((questionContent, index) => (
-              <button
-                key={index}
-                onClick={() => onShowPlanetContent(index)}
-                className="planetsNameButton"
-              >
-                {questionContent.title}
+              <button type="button" onClick={() => toggleQuestion()}>
+                <button
+                  key={index}
+                  onClick={() => onShowPlanetContent(index)}
+                  className="planetsNameButton"
+                >
+                  {questionContent.title}
+
+                  {isQuestionOpen ? 'close' : 'open'}
+                </button>
               </button>
             ))}
+            <div>
+              {isQuestionOpen && (
+                <QuestionCard content={questions[questionIndex].content} />
+              )}
+            </div>
           </ul>
-          <QuestionCard content={questions[questionIndex].content} />
         </div>
       </div>
     </div>
