32696ac6e5aa0ce8ab31ff35608461c8277cade5
AndreiLhu <leahu.andreilucian@gmail.com>
2023-10-30 11:33:00 +0100

    Added Footer Section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,36 @@
+import './Footer.css';
+import logo from '../../../images/logo-bookmark-white.svg';
+import facebook from '../../../images/icon-facebook.svg';
+import twitter from '../../../images/icon-twitter.svg';
+
 const Footer: React.FC = () => {
   return (
-    <div>
-      <p>
-        {' '}
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas culpa
-        quod harum dolor dignissimos, natus dolorem labore asperiores sit error
-        tenetur sunt illo distinctio earum amet quaerat consequatur.
-        Repudiandae, enim.
-      </p>
-      <p>
-        {' '}
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas culpa
-        quod harum dolor dignissimos, natus dolorem labore asperiores sit error
-        tenetur sunt illo distinctio earum amet quaerat consequatur.
-        Repudiandae, enim.
-      </p>
+    <div className="main-footer-div">
+      <div className="logo-links-container">
+        <a href="#">
+          <img
+            src={logo}
+            alt="logo-bookmark"
+            className="footer-logo-bookmark"
+          />
+        </a>
+
+        <nav className="footer-links-container">
+          <a href="#features">FEATURES</a>
+
+          <a href="#pricing">PRICING</a>
+
+          <a href="#contact">CONTACT</a>
+        </nav>
+      </div>
+      <div className="social-icons-container">
+        <a href="https://www.facebook.com/">
+          <img src={facebook} alt="logo-facebook" className="logo-facebook" />
+        </a>
+        <a href="https://twitter.com/">
+          <img src={twitter} alt="logo-twitter" className="logo-twitter" />
+        </a>
+      </div>
     </div>
   );
 };
