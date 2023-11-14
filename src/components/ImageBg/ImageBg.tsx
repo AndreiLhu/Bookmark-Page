1fef301081aefad7594d04d828ba89947ab20e3f
AndreiLhu <leahu.andreilucian@gmail.com>
2023-11-14 15:42:48 +0100

    added footer mobile responsiveness

---
+++ src/components/ImageBg/ImageBg.tsx
@@ -0,0 +1,18 @@
+import './ImageBg.css';
+interface IImageBgProps {
+  image_source: string;
+  content?: any;
+  alt: string;
+  className: string;
+}
+
+const ImageBg = ({ image_source, content }: IImageBgProps) => {
+  return (
+    <div className="imageBg-container">
+      <img src={image_source} alt="Image" className="imageBg-image" />
+      <div className="home-background">{content}</div>
+    </div>
+  );
+};
+
+export default ImageBg;
